1baedb30056309b7e87707e9294dc553c8346daa
dcho0723 <dcho0723@yahoo.com>
2021-07-05 22:24:40 -0400

    started project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const taskList = new taskList();
+
+  let newTaskForm = document.querySelectory('#create-task-form')
+  let newTaskDescription = document.querySelector('#new-task-description')
+  // let newTaskPriority = document.querySelector('#new-task-priority')
+
+  let newTask = document.querySelector('#tasks')
+
+  const renderApp = () => {
+    
+  }
+
+
+  document.querySelector('#create-task-form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    console.log(e)
+
+    // let createTask = {
+
+    // }
+
+  })
 });
